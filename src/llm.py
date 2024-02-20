99596403819f2e8399ccfa04933e5d2810303dbe
Tony Okeke <tonykabilanokeke@gmail.com>
2024-02-20 12:37:02 -0500

    feat: add function for embedding text chunks

---
+++ src/llm.py
@@ -0,0 +1,18 @@
+# src/llm.py
+
+from sentence_transformers import SentenceTransformer
+import torch
+
+from typing import List
+
+from settings import config
+
+encoder = SentenceTransformer(config.EMBEDDING_MODEL)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def embed(texts: List[str], batch_size: int = 64) -> List[List[float]]:
+    embeddings = encoder.encode(
+        texts, show_progress_bar=False, batch_size=batch_size, device=device
+    )
+    return embeddings.tolist()
