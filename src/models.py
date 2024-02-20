3a0c365bde91b23f95ac997c936a02e2084e7321
Tony Okeke <tonykabilanokeke@gmail.com>
2024-02-20 17:24:41 -0500

    feat: add model for atlas vector search

--- src/models.py
+++ src/models.py
@@ -36,7 +36,53 @@ class Lecture(MongoDocument):
 
 
 class LectureChunk(MongoDocument):
-    subject_id: ObjectId
-    lecture_id: ObjectId
+    subject_id: str
+    lecture_id: str
     chunk: str
     embedding: List[float]
+
+
+class AtlasVectorSearch(BaseModel):
+    index: str = "lecture-index"
+    path: str = "embedding"
+    query_vector: List[float]
+    n_candidates: int
+    top_k: int
+    subject_filter: List[str]
+    lecture_filter: List[str]
+
+    def mkpipeline(self):
+        """Construct pipeline for Atlas Search"""
+        pipeline = [
+            {
+                "$vectorSearch": {
+                    "index": self.index,
+                    "path": self.path,
+                    "queryVector": self.query_vector,
+                    "numCandidates": self.n_candidates,
+                    "limit": self.top_k,
+                }
+            },
+            {
+                "$project": {
+                    "_id": 0,
+                    "subject_id": 1,
+                    "lecture_id": 1,
+                    "chunk": 1,
+                    "score": {"$meta": "vectorSearchScore"},
+                }
+            },
+        ]
+
+        filter_ = []
+        if self.subject_filter != []:
+            filter_.append({"subject_id": {"$in": self.subject_filter}})
+        if self.lecture_filter != []:
+            filter_.append({"lecture_id": {"$in": self.lecture_filter}})
+
+        if len(filter_) == 1:
+            pipeline[0]["$vectorSearch"]["filter"] = filter_[0]
+        elif len(filter_) == 2:
+            pipeline[0]["$vectorSearch"]["filter"] = {"$or": filter_}
+
+        return pipeline
