b436a65cdeceaa50c0aaf083c7711b76f123cd69
Tony Okeke <tonykabilanokeke@gmail.com>
2024-02-20 12:37:53 -0500

    feat: add command to chunk and embed lectures

--- scripts/seed_db.py
+++ scripts/seed_db.py
@@ -2,19 +2,24 @@
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
-from rich import print
-from tqdm import tqdm
 import asyncio
 
-from typing import Tuple, List
+from llama_index.core.node_parser import MarkdownNodeParser
+from llama_index.readers.file import FlatReader
+
+from rich import print
+from tqdm import tqdm
+
+from typing import Dict, Tuple, List
 from pathlib import Path
 import sys
 import re
 import aiofiles
 
 sys.path.append("src")
-from models import Subject, Lecture
+from models import Subject, Lecture, LectureChunk
 from settings import config
+from llm import embed
 
 
 async def get_lectures() -> List[Path]:
@@ -55,8 +60,9 @@ async def init_database(db):
     await db["lectures"].create_index([("idx", 1)], unique=True)
 
 
-async def process_lectures(db, lectures):
+async def process_lectures(db, lectures) -> Dict:
     subjects = {}
+    lecture_info = {}
 
     for lecture in tqdm(lectures, total=len(lectures), desc="seeding database"):
         idx, subject, title = await extract_metadata(lecture)
@@ -64,14 +70,19 @@ async def process_lectures(db, lectures):
 
         if subject not in subjects:
             subject_document = Subject(title=subject, lectures=[]).dump("upload")
-            result = await db["subjects"].insert_one(subject_document)
-            subjects[subject] = {"id": result.inserted_id, "lectures": []}
+            res_s = await db["subjects"].insert_one(subject_document)
+            subjects[subject] = {"id": res_s.inserted_id, "lectures": []}
 
         lecture_document = Lecture(
-            idx=idx, title=title, subject=subjects[subject]["id"], content=content
+            idx=idx, title=title, subject_id=subjects[subject]["id"], content=content
         ).dump("upload")
-        result = await db["lectures"].insert_one(lecture_document)
-        subjects[subject]["lectures"].append(result.inserted_id)
+        res_l = await db["lectures"].insert_one(lecture_document)
+
+        subjects[subject]["lectures"].append(res_l.inserted_id)
+        lecture_info[str(lecture)] = {
+            "subject": res_s.inserted_id,
+            "lecture": res_l.inserted_id,
+        }
 
     for subject, details in tqdm(
         subjects.items(), total=len(subjects), desc="updating subjects"
@@ -80,14 +91,48 @@ async def process_lectures(db, lectures):
             {"_id": details["id"]}, {"$set": {"lectures": details["lectures"]}}
         )
 
+    return lecture_info
+
+
+async def chunk_and_embed(db, lecture_info):
+    reader = FlatReader()
+    parser = MarkdownNodeParser()
+
+    for filepath, identifiers in tqdm(
+        lecture_info.items(),
+        total=len(lecture_info),
+        desc="chunking & embedding lectures...",
+    ):
+        doc = reader.load_data(Path(filepath))
+        chunks = [
+            chk.text for chk in parser.get_nodes_from_documents(doc) if chk.text != ""
+        ]
+        embeddings = embed(chunks)
+
+        lecture_chunks = [
+            LectureChunk(
+                subject_id=identifiers["subject"],
+                lecture_id=identifiers["lecture"],
+                chunk=chk,
+                embedding=embedding,
+            ).dump("upload")
+            for chk, embedding in zip(chunks, embeddings)
+        ]
+
+        await db["lecture_chunks"].insert_many(lecture_chunks)
+
 
 async def main():
-    print("[green]starting database seeding...[/green]")
+    print("[cyan]seeding 'lectures' and 'subjects' collections[/cyan]")
     client = AsyncIOMotorClient(config.MONGO_URI, server_api=ServerApi("1"))
     db = client["abokicode_db"]
     await init_database(db)
     lectures = await get_lectures()
-    await process_lectures(db, lectures)
+    lecture_info = await process_lectures(db, lectures)
+    print("[green]done 😊[/green]", end="\n\n")
+
+    print("[cyan]seeding lecture_chunks[/cyan]")
+    await chunk_and_embed(db, lecture_info)
     print("[green]done 😊[/green]")
 
 
